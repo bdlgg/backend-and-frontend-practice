f013d5153cdab89188de07fd6b092dbaa9a4c3e9
aleksejkonstantinov <40akka@gmail.com>
2025-09-18 18:33:06 +0300

    add: добавил функцию проверки номера телефона

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 const form=document.getElementById('ContactForm');  //получаем элемент формы
 const emailInput=document.getElementById('Email');//получаем поле ввода email
+const phoneInput=document.getElementById('phone');
 const button=document.getElementById('Submit');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  validateEmail()
+  const emailValid = validateEmail();
+  const phoneValid = validatePhone();
+  if (emailValid && phoneValid) {
+    alert("Форма успешно отправлена!");
+  }
 }); //на событие ввода email будет вызываться функция validateEmail()
 
 function validateEmail(){
@@ -12,11 +17,21 @@ function validateEmail(){
   const emailRegex=/[A-Za-z0-9_\-\.]{2,10}@[a-z0-9\.\-_]{1,10}\.[a-z]{2,3}/;
   if(emailRegex.test(emailInput.value)){
     removeError(emailInput);
-    return True;
-
+    return true;
   }
   else{
     showError(emailInput, "Email не соответствует формату. Пример: ivanov@mail.ru");
+    return false;
+  }
+}
+function validatePhone(){
+  const phoneRegex = /^(\+7|8)[\s-]?\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{2}[\s-]?\d{2}$/;
+  if(phoneRegex.test(phoneInput.value)){
+    removeError(phoneInput);
+    return true;
+  }
+  else{
+    showError(phoneInput, "Телефон не соответствует формату. пример: +7(999)123-45-67");
     return false;
   }
 }
